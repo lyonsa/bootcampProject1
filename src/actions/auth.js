33f230af07dc948c58f1d50de8cb4649ad114703
Charles Kenney <Charlesc.kenney@gmail.com>
2017-11-13 22:35:00 -0500

    add fb user presence listener

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase'
 import { routerActions } from 'react-router-redux'
 
-import { firebaseAuth, firebasePlayers } from '../firebase'
+import { firebaseAuth, firebasePlayers, firebaseDb } from '../firebase'
 import { isNewUser } from '../utils'
 
 // action types
@@ -10,6 +10,8 @@ export const SIGN_IN_ERROR = 'SIGN_IN_ERROR'
 export const SIGN_OUT_SUCCESS = 'SIGN_OUT_SUCCESS'
 export const SETUP_NEW_USER_SUCCESS = 'SETUP_NEW_USER_SUCCESS'
 export const SETUP_NEW_USER_ERROR = 'SETUP_NEW_USER_ERROR'
+export const SET_USER_PRESENCE_SUCCESS = 'SET_USER_PRESENCE_SUCCESS'
+export const SET_USER_PRESENCE_ERROR = 'SET_USER_PRESENCE_ERROR'
 export const SET_USER_PROFILE_SUCCESS = 'SET_USER_PROFILE_SUCCESS'
 export const SET_USER_PROFLE_ERROR = 'SET_USER_PROFILE_ERROR'
 export const UPDATE_USER_PROFILE_SUCCESS = 'UPDATE_USER_PROFILE_SUCCESS'
@@ -40,6 +42,15 @@ export const setupNewUserSuccess = () => ({
 
 export const setupNewUserError = err => ({
   type: SETUP_NEW_USER_ERROR,
+  payload: err,
+})
+
+export const setUserPresenceSuccess = () => ({
+  type: SET_USER_PRESENCE_SUCCESS,
+})
+
+export const setUserPresenceError = err => ({
+  type: SET_USER_PRESENCE_ERROR,
   payload: err,
 })
 
@@ -106,9 +117,12 @@ export const authenticate = (provider) => {
       dispatch(signInSuccess(user))
       // setup newly registered users
       if (isNewUser(user)) await dispatch(setupNewUser())
-      // get user profile
-      await dispatch(setUserProfile(uid)) 
+      // set user presence
+      await dispatch(setUserPresence(uid))      
+      // set user profile
+      await dispatch(setUserProfile(uid))
       // redirect to home
+      await dispatch(routerActions.push('/'))
       // watch user profile for changes
       await dispatch(watchUserProfile(uid))
     } catch (err) {
@@ -133,9 +147,35 @@ export const signOut = () => {
   }
 }
 
+export const setUserPresence = uid => {
+  return async dispatch => {
+    try {
+      // get ref
+      const isOnline = firebaseDb.ref('.info/connected')
+      // register user presence callback
+      const onlineRef = await firebaseDb.ref(`players/${uid}/online`)
+      isOnline.on('value', snap => dispatch(updateUserPresence(snap, onlineRef)))
+      dispatch(setUserPresenceSuccess())
+    } catch (err) {
+      console.error(`Error setting user presence: ${err}`)
+      dispatch(setUserPresenceError(err))
+    }
+  }
+}
+
+export const updateUserPresence = (snap, ref) => {
+  return async dispatch => {
+    if (snap.val()) {
+      ref.onDisconnect().remove()
+      ref.set(true)
+    }
+  }
+}
+
 export const setUserProfile = uid => {
   return async dispatch => {
     try {
+      // get profile
       const ref = firebasePlayers.child(uid)
       const snap = await ref.once('value')
       const profile = snap.val()
