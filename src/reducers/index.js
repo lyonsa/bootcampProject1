5499631d4c459e833292d882e1d255725aff2f3a
Charles Kenney <Charlesc.kenney@gmail.com>
2017-11-16 15:10:18 -0500

    add layout reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,12 @@ import { routerReducer } from 'react-router-redux'
 import auth from './auth'
 import queue from './queue'
 import game from './game'
+import layout from './layout'
 
 export default combineReducers({
   router: routerReducer,
   auth,
   queue,
   game,
+  layout,
 })
